d7bf5c811396793e91102fc024c4780eb5e48760
nijhazer-sct <strapi.cloud.trial@onefourtwo.io>
2024-06-12 13:34:38 -0400

    Adding basic post group content type

---
+++ src/api/post-group/controllers/post-group.ts
@@ -0,0 +1,7 @@
+/**
+ * post-group controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::post-group.post-group');
---
+++ src/api/post-group/routes/post-group.ts
@@ -0,0 +1,7 @@
+/**
+ * post-group router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::post-group.post-group');
